1b2bb64a58f3952b2c381949e9cad4d9ab6e6592
Maiara Muller <maiara.15muller@outlook.com>
2023-10-24 19:20:51 -0300

    menu

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 let boddy = document.querySelector("body");
-let inicio = document.querySelector(".inicio")
-let imagem = document.querySelector("#imagem")
-let container = document.querySelector(".container")
-let h1 = document.querySelector("h1")
-let color = document.querySelector(".color")
-const teste = "#f0ce4e"
+let menu = document.querySelector(".topbar");
 
 const trocarCor = () => {
-    boddy.classList.toggle("dark")
-}
+  boddy.classList.toggle("dark");
+  /*   boddy.classList.toggle("white"); */
+  menu.classList.toggle("dark");
+};
 
-/* button.addEventListener("click", () =>{
-    button.classList.toggle("dark")
-    inicio.classList.toggle("dark")
-    imagem.classList.toggle("dark")
-    container.classList.toggle("dark")
-    h1.classList.toggle("dark")
-    color.classList.toggle("dark")
-}) */
-
-
-
-
+const menuResponsivo = () => {
+  document.querySelector(".topbar").classList.toggle("responsive");
+};
